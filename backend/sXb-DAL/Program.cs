7ef00403db6e93cb8d9bfb030472e496ce4f6419
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-08-26 08:06:37 -0400

    Initialized DAL project

---
+++ backend/sXb-DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sXb_DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
