dc957996026366a8b07472342a2f3d079bad70fa
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-09-06 09:30:13 -0400

    CircleCI: Fixed frontend test errors.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing (react-create-app)', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>, div);
-  await new Promise(resolve => setTimeout(resolve, 1000));
 });
