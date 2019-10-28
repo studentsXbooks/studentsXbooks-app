c6ca487a98977fedbd588f6ea8a58eb17d510624
Brady <bradystarcher@hotmail.com>
2019-10-28 12:37:45 -0400

    Added more Copy

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,13 +2,42 @@ import React, { useState } from "react";
 
 export default () => (
   <div>
-    <p>What our site does to help students</p>
-    <p>
-      We want to improve the way students can purchase their textbooks for their
-      classes, as well as allow students an easier way to offload books they no
-      longer need or want to keep around. We want our site to make it easier and
-      more cost effective for students so that they can make their dreams a
-      reality in the future.
-    </p>
+    <div>
+      <p>What our site does to help students</p>
+      <p>
+        We want to improve the way students can purchase their textbooks for
+        their classes, as well as allow students an easier way to offload books
+        they no longer need or want to keep around. We want our site to make it
+        easier and more cost effective for students so that they can make their
+        dreams a reality in the future.
+      </p>
+    </div>
+    <div>
+      <p>Our story and who we are</p>
+      <p>
+        {" "}
+        We are a group of software engineering students from West Virginia
+        University at Parkersburg who created Studentxbooks originally as a
+        senior project for our class, but now that project has evolved and is
+        now a site that aims to help students out on dealing with the ever
+        rising price of textbooks.
+      </p>
+      <p>The Team</p>
+      <p>
+        Levi Butcher - Project Manager <br></br> Sean Rickard - Developer{" "}
+        <br></br> Jeremi Swann - Developer <br></br> Brady Starcher -
+        Documenter/Designer <br></br> Samantha Burkey - Tester/Designer
+      </p>
+    </div>
+    <div>
+      <p>Our College!</p>
+      <p>About the college:</p>
+      <p>
+        Our college is West Virginia University at Parkersburg<br></br> The
+        current president of the college is Chris Gilmer<br></br> The Software
+        Engineering Professors that have lead the way in our learning are
+        Charles Almond and Gary Thompson
+      </p>
+    </div>
   </div>
 );
--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -2,9 +2,30 @@ import React, { useState } from "react";
 
 export default () => (
   <div>
-    <p>
-      Need help with navigating the site? Have a bug that needs to be reported?
-      Look no further!
-    </p>
+    <div>
+      <p>
+        Need help with navigating the site? Have a bug that needs to be
+        reported? Look no further!
+      </p>
+    </div>
+    <div>
+      <p>FAQ</p>
+      <p>
+        How does the site gather the final price for a selling/buying of a book?
+      </p>
+      <p>Can I buy a book while one of my other books is still on sale?</p>
+    </div>
+    <div>
+      <p>Developer Contacts/Helpful Links</p>
+      <p>
+        If you have you have issues with the site you can contact us through our
+        email: Studentxbooks@gmail.com
+      </p>
+      <p>
+        For bugs, errors, or to give suggestions for improvement on our site you
+        can create an issue at our GitHub repo:
+        https://github.com/studentsXbooks
+      </p>
+    </div>
   </div>
 );
