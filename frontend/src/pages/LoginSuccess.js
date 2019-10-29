d369fc9a6c4d70ff5cfd2a5322933b9e28273057
Jeremi Swann <1616369+jeremii@users.noreply.github.com>
2019-10-29 15:18:53 -0400

    login redirect

--- frontend/src/pages/LoginSuccess.js
+++ frontend/src/pages/LoginSuccess.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 // TODO: Consider changing this page to self-dismissing modal popup.
 
 const LoginSuccess = () => {
-  setTimeout(() => {
-    window.location.href = "/home";
-  }, 1500);
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "/home";
+    }, 1500);
+  });
 
   return <h1 style={{ color: "blue" }}>Welcome!</h1>;
 };
