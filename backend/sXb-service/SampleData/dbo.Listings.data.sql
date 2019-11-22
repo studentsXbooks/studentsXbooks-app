7b4b060e59919dbd20d506d6bad554dc912a3fad
Sean <sean.rickard26@gmail.com>
2019-11-22 12:16:47 -0500

    update sample data

--- backend/sXb-service/SampleData/dbo.Listings.data.sql
+++ backend/sXb-service/SampleData/dbo.Listings.data.sql
@@ -1,152 +1,214 @@
-﻿INSERT [dbo].[Listings]   
+﻿INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES     
-	(N'461dcea3-d85b-3cb9-85ae-763a0ff1ff74', N'a1ac9d52-8a92-4891-b8c7-f1d64051accc', N'2112', CAST(9.13 AS Decimal(18, 2)), 2, 1)  
-INSERT [dbo].[Listings] 
+VALUES
+	(N'461dcea3-d85b-3cb9-85ae-763a0ff1ff74', N'a1ac9d52-8a92-4891-b8c7-f1d64051accc', N'2112', CAST(9.13 AS Decimal(18, 2)), 2, 1)
+INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES     
+VALUES
 	(N'66ef86f0-1d41-d2bb-0374-78b45ed0e9b3', N'c4b79351-2821-41ec-a470-377aa2a4cfae', N'2112', CAST(262.14 AS Decimal(18, 2)), 4, 1)
-INSERT [dbo].[Listings] 
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'e26f5e19-a467-f81c-c184-d14e533436d9', N'66cccd3b-55da-4ddf-9f99-8b121bb35ca9', N'1234', CAST(162.22 AS Decimal(18, 2)), 3, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'557c5c19-7f90-6b4f-6cca-d4625637bef8', N'cca84cd8-5e2e-4345-b9a7-24e9130f3864', N'1234', CAST(217.87 AS Decimal(18, 2)), 4, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'93fb168f-6b68-05ea-60f5-d7da4720a9af', N'69280bd1-b21b-4e59-a735-d2a165494a2c', N'1234', CAST(22.17 AS Decimal(18, 2)), 3, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'ee2aad7b-42d6-1491-f11a-d9eef4dde9e4', N'82b8bb64-42ff-4f73-b8dd-27caaee9ec2d', N'2112', CAST(146.55 AS Decimal(18, 2)), 4, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'bee307aa-fb1d-151c-01d1-dacd4c7f28fd', N'3a52ab67-3093-4dd8-942a-a69c6364a357', N'2112', CAST(32.16 AS Decimal(18, 2)), 3, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'af17d9b6-ebd5-897e-baff-df33d7e027fd', N'221fa085-34c0-4f71-83b1-cf4fd2e13411', N'2112', CAST(6.82 AS Decimal(18, 2)), 2, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'870551a0-0c57-ddde-6d9c-e108a95e47b5', N'cd2062cc-dd1f-4e07-b863-44e9e3f15dad', N'2112', CAST(182.12 AS Decimal(18, 2)), 2, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'0ec9466c-f79d-2c40-b6c4-e3c7b04a6d09', N'6814dc0d-ab04-4059-a4e8-6ea9fc3131d0', N'2112', CAST(210.85 AS Decimal(18, 2)), 2, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'13ff38c2-ae7e-a45e-ec9d-e4e828cb5ade', N'83c22db5-d88b-40d5-bce1-f3cf976c9b43', N'2112', CAST(253.07 AS Decimal(18, 2)), 2, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'903e4ee5-9c3f-8750-b935-e557fc1cbd53', N'0b6fd204-aaad-4a49-bcbc-6ae342cfca57', N'2112', CAST(13.40 AS Decimal(18, 2)), 1, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'a96caaa3-f832-796e-135e-e734211ec15e', N'0cd91468-28fa-4010-b1c3-19e934d9ca2d', N'9112', CAST(260.31 AS Decimal(18, 2)), 1, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'715fa9fd-3b70-fa53-391b-e78246f34eee', N'8769f511-e9f6-4ccb-b470-98d3993c468d', N'9112', CAST(67.84 AS Decimal(18, 2)), 2, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'b50a3205-9786-32ac-3ad1-ead6be093257', N'e4122962-ab10-464f-ab63-b9d11ccfdbe6', N'9112', CAST(38.80 AS Decimal(18, 2)), 4, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'0b6e0ba7-bf04-5a5f-b8ec-f1c726108685', N'a58211ca-77f2-4248-bbf4-be5c5a111a95', N'9112', CAST(185.55 AS Decimal(18, 2)), 3, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
-
-([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES (N'782f5bf2-23fb-a031-d1f5-f73f8376857d', N'ed9b216d-db90-4dfc-b455-3b6293420785', N'9112', CAST(64.38 AS Decimal(18, 2)), 1, 1)
- 
-
-
-
-INSERT [dbo].[Listings] 
+INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES 
+VALUES
+	(N'e26f5e19-a467-f81c-c184-d14e533436d9', N'66cccd3b-55da-4ddf-9f99-8b121bb35ca9', N'1234', CAST(162.22 AS Decimal(18, 2)), 3, 1)
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'557c5c19-7f90-6b4f-6cca-d4625637bef8', N'cca84cd8-5e2e-4345-b9a7-24e9130f3864', N'1234', CAST(217.87 AS Decimal(18, 2)), 4, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'93fb168f-6b68-05ea-60f5-d7da4720a9af', N'69280bd1-b21b-4e59-a735-d2a165494a2c', N'1234', CAST(22.17 AS Decimal(18, 2)), 3, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'ee2aad7b-42d6-1491-f11a-d9eef4dde9e4', N'82b8bb64-42ff-4f73-b8dd-27caaee9ec2d', N'2112', CAST(146.55 AS Decimal(18, 2)), 4, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'bee307aa-fb1d-151c-01d1-dacd4c7f28fd', N'3a52ab67-3093-4dd8-942a-a69c6364a357', N'2112', CAST(32.16 AS Decimal(18, 2)), 3, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'af17d9b6-ebd5-897e-baff-df33d7e027fd', N'221fa085-34c0-4f71-83b1-cf4fd2e13411', N'2112', CAST(6.82 AS Decimal(18, 2)), 2, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'870551a0-0c57-ddde-6d9c-e108a95e47b5', N'cd2062cc-dd1f-4e07-b863-44e9e3f15dad', N'2112', CAST(182.12 AS Decimal(18, 2)), 2, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'0ec9466c-f79d-2c40-b6c4-e3c7b04a6d09', N'6814dc0d-ab04-4059-a4e8-6ea9fc3131d0', N'2112', CAST(210.85 AS Decimal(18, 2)), 2, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'13ff38c2-ae7e-a45e-ec9d-e4e828cb5ade', N'83c22db5-d88b-40d5-bce1-f3cf976c9b43', N'2112', CAST(253.07 AS Decimal(18, 2)), 2, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'903e4ee5-9c3f-8750-b935-e557fc1cbd53', N'0b6fd204-aaad-4a49-bcbc-6ae342cfca57', N'2112', CAST(13.40 AS Decimal(18, 2)), 1, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'a96caaa3-f832-796e-135e-e734211ec15e', N'0cd91468-28fa-4010-b1c3-19e934d9ca2d', N'9112', CAST(260.31 AS Decimal(18, 2)), 1, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'715fa9fd-3b70-fa53-391b-e78246f34eee', N'8769f511-e9f6-4ccb-b470-98d3993c468d', N'9112', CAST(67.84 AS Decimal(18, 2)), 2, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'b50a3205-9786-32ac-3ad1-ead6be093257', N'e4122962-ab10-464f-ab63-b9d11ccfdbe6', N'9112', CAST(38.80 AS Decimal(18, 2)), 4, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'0b6e0ba7-bf04-5a5f-b8ec-f1c726108685', N'a58211ca-77f2-4248-bbf4-be5c5a111a95', N'9112', CAST(185.55 AS Decimal(18, 2)), 3, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+	(N'782f5bf2-23fb-a031-d1f5-f73f8376857d', N'ed9b216d-db90-4dfc-b455-3b6293420785', N'9112', CAST(64.38 AS Decimal(18, 2)), 1, 1)
+
+
+
+
+INSERT [dbo].[Listings]
+	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
 	(N'3b780da2-ca7c-484b-df7c-f86534f5adcc', N'79ade74e-4703-4022-b709-da5f1c43d5a1', N'1234', CAST(272.63 AS Decimal(18, 2)), 2, 1)
 
-INSERT [dbo].[Listings] 
+INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
 VALUES
 	(N'656d3201-5904-e5bf-4413-fbf604bf0fa1', N'c91cdbed-67a7-460f-9297-984e47ce4432', N'1234', CAST(271.76 AS Decimal(18, 2)), 3, 1)
- 
-INSERT [dbo].[Listings] 
+
+INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES 
+VALUES
 	(N'c4617230-adc2-8fbd-72a9-fcc66f58c698', N'0280f98f-3f72-4fa6-804f-160a3b2c6049', N'1234', CAST(232.94 AS Decimal(18, 2)), 4, 1)
- 
-INSERT [dbo].[Listings] 
+
+INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
-VALUES 
+VALUES
 	(N'93f8289e-1ab0-6bac-1987-fe2046870245', N'70340edd-3a13-4c5c-a268-c2d106778f96', N'1234', CAST(221.49 AS Decimal(18, 2)), 3, 1)
 
 INSERT [dbo].[Listings]
 	([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
 VALUES
 	(N'1b5cab6a-b888-1a04-fd03-feff3b8c78b4', N'b3da9f62-74fb-4bc6-80c4-5b7fbb2b83d9', N'1234', CAST(207.57 AS Decimal(18, 2)), 2, 1)
- 
+ INSERT INTO [dbo].[Listings]
+    ([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+    (N'a059efc3-a4ec-4abf-946a-84194b2e0a00',
+	 N'32eca70d-41c6-4ef9-8ac2-31ce07a3b67c',
+	 N'1234', 
+	 10.99, 
+	 0,1)
+INSERT INTO [dbo].[Listings]
+    ([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+    (N'a059efc3-a4ec-4abf-946a-84194b2e0a01',
+     N'32eca70d-41c6-4ef9-8ac2-31ce07a3b67d',
+     N'1234', 
+     12.99, 
+     0,1)
+INSERT INTO [dbo].[Listings]
+    ([Id], [BookId], [UserId], [Price], [Condition], [ContactOption])
+VALUES
+    (N'a059efc3-a4ec-4abf-946a-84194b2e0a02',
+     N'bff41fc5-31ce-4e62-ae93-96ce81b20c06',
+     N'1234', 
+     2.99, 
+     0,1)
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption])
+values
+(N'70b68e60-9c07-4d6b-bf91-774e27b016df',
+N'BFF41FC5-31CE-4E62-AE93-96CE81B20C08', N'1234', 67.65, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'210ca506-d0ac-47d7-9724-f004c19d18df', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C31'), N'1234', 48.25, 4,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'2b9b2eab-6ce9-4cf4-bfbc-2f6c2859a3c3', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C07'), N'1234', 79.45, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'2b515abe-1c90-46ff-870f-6543fb7c2d23', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C60'), N'1234', 72.62, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'3b19aacb-dbd2-4193-863d-c733e0abdca3', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C87'), N'1234', 76.81, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'f578e020-34ee-45aa-91b4-f459ecc867f2', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C63'), N'1234', 38.17, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'dc2f75d3-81e5-4a10-a0dc-18cc5008f403', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C29'), N'1234', 19.03, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'5bf03cf3-ea25-433d-8c67-51c7fade6ff3', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C53'), N'1234', 10.21, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'bfa7ebb2-b474-49d4-a89e-f88c79de9a9f', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C15'), N'1234', 52.96, 1,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'1f344762-77c0-4440-b9a6-6140686a4f55', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C73'), N'1234', 89.35, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'a022dfc2-6471-49ff-969b-76911a2ffa8f', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C52'), N'1234', 1.97, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'4fa882e2-afc5-4180-bf90-c0f8b2bc8df7', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C44'), N'1234', 85.21, 1,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'702b35d7-fdc4-4c19-93e9-5dbcbb4e7768', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C18'), N'1234', 14.67, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'c6891c77-262f-4b5b-b2dc-de89488769ec', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C45'), N'1234', 45.83, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'464cc1bf-14ed-4662-a9b9-53d806720a64', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C55'), N'1234', 74.66, 4,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'060f6800-3692-430f-b54b-e1251c90e444', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C09'), N'1234', 3.85, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'02adfabf-a2ba-43bd-bdce-7bcf5411684c', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C46'), N'1234', 45.46, 1,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'c0ed2987-7a83-443f-b4f3-75f3b26d95ec', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C32'), N'1234', 83.67, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'209a2e4a-ce0e-40c9-a84c-fc0d32196d26', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C34'), N'1234', 63.49, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'2bf40e91-9fac-4d3b-bde4-0a077f538471', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C62'), N'1234', 89.03, 3,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'ef507cb4-1230-4e30-a4be-d637f539b312', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C25'), N'1234', 53.26, 2,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'c3417bff-98eb-4e07-a9dd-e83a3f77cd3c', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C37'), N'1234', 45.0, 1,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'01871317-a38c-4de4-9185-095ae3969fc4', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C84'), N'1234', 67.82, 4,1);
+insert into Listings (Id, BookId, UserId, Price, Condition, [ContactOption]) values (N'2b862064-a1e3-4713-985c-71ceb6c1e68b', CONVERT(uniqueidentifier, N'BFF41FC5-31CE-4E62-AE93-96CE81B20C97'), N'1234', 4.94, 2,1);
