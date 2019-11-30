8750b73b72eac4034c139ebca29a9cb8e217c20a
Brady <bradystarcher@hotmail.com>
2019-11-30 18:51:58 -0500

    Using Grid to Position Search

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,6 +8,9 @@ import SearchIcon from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
 import Button from "@material-ui/core/Button";
 import { navigate } from "@reach/router";
+import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 
 const SearchBox = styled.div`
   margin-bottom: 2rem;
@@ -16,9 +19,6 @@ const SearchBox = styled.div`
   flex-flow: row nowrap;
   grid-template: "logo search" auto / 20% 80%;
   align-items: center;
-  position: absolute;
-  left: 400px;
-  top: 330px;
   color: white;
 `;
 
@@ -33,10 +33,29 @@ const LogoArea = styled.div`
   grid-area: logo;
 `;
 
+const SearchLayout = styled.div`
+  display: grid;
+  /* grid-template-rows: auto;
+  grid-template-columns: 50% 50%; */
+  grid-template-columns: 20% 20% 20% 20% 20%;
+  grid-template-rows: 20% 20% 20% 20% 20%;
+  grid-row-gap: 60px;
+  justify-items: center;
+  align-items: center;
+`;
+
+const SearchPosition = styled.div`
+  display: grid;
+  grid-row-start: 3;
+  grid-column-end: 3;
+  grid-gap: 60px;
+  align-items: center;
+`;
+
 const SearchForm = styled.form`
   border: 2px solid #3f51b5;
   border-radius: 25px;
-  border-color: #707070;
+  border-color: white;
   height: 43px;
   width: 500px;
   display: grid;
@@ -45,22 +64,34 @@ const SearchForm = styled.form`
   justify-content: space-between;
   position: absolute;
   color: white;
+  position: relative;
+  z-index: 2;
+  background: white;
+
   & > button {
     align-self: stretch;
     width: 150px;
     border-radius: 25px;
     background-color: #33578c;
-    //position: absolute;
-    // left: 900px;
-    // top: 300px;
   }
   & > div {
     padding-left: 1rem;
+  }
+  & input {
+    color: gray !important;
+    background: white;
+  }
+  & input::placeholder {
+    color: gray !important;
+    opacity: 1;
+    background: white;
   }
 `;
 const BkgOverlay = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   height: 100%;
+  position: relative;
+  z-index: 1;
 `;
 const MainContent = styled.div`
   background-image: url("./BookShelf2.JPG");
@@ -79,37 +110,50 @@ const Home = () => {
   return (
     <MainContent>
       <BkgOverlay>
-        {/* <fyt>Find your textbooks!</fyt> */}
-        <SearchBox>
-          <SearchArea>
-            <SearchForm
-              method="POST"
-              onSubmit={e => {
-                e.preventDefault();
-                navigate(`/search/${search}`);
-              }}
-            >
-              <InputBase
-                placeholder="Title, Author, ISBN..."
-                onChange={e => setSearch(e.target.value)}
-                inputProps={{ "aria-label": "search" }}
-              />
-              <Button
-                type="submit"
-                startIcon={<SearchIcon />}
-                color="primary"
-                variant="contained"
-              >
-                Search
-              </Button>
-            </SearchForm>
-          </SearchArea>
-        </SearchBox>
+        <SearchLayout>
+          <SearchPosition>
+            <SearchBox>
+              <SearchArea>
+                <SearchForm
+                  method="POST"
+                  onSubmit={e => {
+                    e.preventDefault();
+                    navigate(`/search/${search}`);
+                  }}
+                >
+                  <InputBase
+                    placeholder="Title, Author, ISBN..."
+                    onChange={e => setSearch(e.target.value)}
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                  <Button
+                    type="submit"
+                    startIcon={<SearchIcon />}
+                    color="primary"
+                    variant="contained"
+                  >
+                    Search
+                  </Button>
+                </SearchForm>
+              </SearchArea>
+            </SearchBox>
+          </SearchPosition>
+        </SearchLayout>
       </BkgOverlay>
     </MainContent>
   );
 };
 
 export default Home;
+
+{
+  /* <MuiThemeProvider theme={input}>
+                <InputBase
+                  placeholder="Title, Author, ISBN..."
+                  onChange={e => setSearch(e.target.value)}
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </MuiThemeProvider> */
+}
 
 // style={{ backgroundImage: "url(${./BookShelf.JPG})" }}
