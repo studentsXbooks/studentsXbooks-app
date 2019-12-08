3c582f8991c286065a0cb60e42b038f5abd8ff9e
Levi Butcher <levibutcher355@gmail.com>
2019-12-08 13:58:12 -0500

    Create listing validation

--- frontend/src/pages/ListingDetails.js
+++ frontend/src/pages/ListingDetails.js
@@ -6,7 +6,6 @@ import {
   Dialog,
   DialogContent
 } from "@material-ui/core";
-// $FlowFixMe
 import styled from "styled-components";
 import useApi from "../hooks/useApi";
 import ContactSellerForm from "../components/ContactSellerForm";
@@ -43,7 +42,10 @@ const ListingDetails = ({ id }: Props) => {
             <ImageBox>
               <img src={listing.thumbnail} alt="Book" />
               <Typography variant="h4" align="left">
-                ISBN: {listing.isbn10}
+                ISBN-10: {listing.isbn10}
+              </Typography>
+              <Typography variant="h4" align="left">
+                ISBN-13: {listing.isbn13 || "None"}
               </Typography>
             </ImageBox>
           </Grid>
--- frontend/src/ui/Input.js
+++ frontend/src/ui/Input.js
@@ -1,11 +1,23 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
+import { ErrorMessage } from "formik";
+import styled from "styled-components";
+import type { FieldProps } from "formik";
 
-type Props = {
-  field: {},
-  props: {}
-};
-const Input = ({ field, ...props }: Props) => (
-  <TextField {...field} {...props} />
+const ErrorText = styled.span`
+  color: red;
+`;
+
+const Input = ({ field, ...props }: FieldProps) => (
+  <TextField
+    {...field}
+    {...props}
+    helperText={
+      <ErrorMessage
+        name={field.name}
+        render={msg => <ErrorText>{msg}</ErrorText>}
+      />
+    }
+  />
 );
 export default Input;
